956fb9928a270642480b7cc0e6f987ec4b936498
wintaye <41543902+wintaye@users.noreply.github.com>
2022-07-28 00:47:39 -0500

    uploaded end of day

---
+++ src/test/java/springbootapi/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootapi.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
